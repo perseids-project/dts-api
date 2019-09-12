09d3842f9539d8cb5535d1724734f81c408b2fef
zfletch <zfletch2@gmail.com>
2019-09-12 17:45:16 -0400

    use nginx to cache requests

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,9 @@ Rails.application.configure do
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
+  # Allow other applications in Docker Compose to refer to this as 'app'
+  config.hosts << 'app'
+
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
